4478926117a56d8a7abe52e816b9e6bc63dec4c5
momin-khan1 <momingit3@gmail.com>
2023-07-26 11:35:09 +0600

    part 01

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-
+const items = [1, 2, 3, 4, 5, 6]
 </script>
 
 <template>
-  
+  <section
+    class="mx-auto container flex items-center text-center flex-col space-y-2"
+  >
+    <div class="p-5 border border-gray-600 w-40" v-for="(item, index) in items" :key="index">{{ item }}</div>
+  </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
