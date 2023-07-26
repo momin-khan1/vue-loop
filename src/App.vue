7825359bc1d069e03babe5afb28120e2be1f43d8
momin-khan1 <momingit3@gmail.com>
2023-07-26 11:47:32 +0600

    Part 02

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
-const items = [1, 2, 3, 4, 5, 6]
+const tasks = [
+  "Complete project proposal",
+  "Update website content",
+  "Fix bug in user authentication",
+  "Prepare presentation slides",
+  "Test new feature implementation",
+];
+
 </script>
 
 <template>
-  <section
-    class="mx-auto container flex items-center text-center flex-col space-y-2"
-  >
-    <div class="p-5 border border-gray-600 w-40" v-for="(item, index) in items" :key="index">{{ item }}</div>
+  <section class="mx-auto container">
+    <section class="mx-auto container space-y-3">
+      <div
+        class="p-5 border border-gray-600 text-left" v-for="(task, index) in tasks" key="index">
+        {{ index + 1 }} - {{ task }}
+      </div>
+    </section>
   </section>
 </template>
 
