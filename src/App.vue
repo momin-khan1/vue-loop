091e5179012a8a739359a13043c582cc3a663135
codermomin1 <momingithub@gmail.com>
2023-07-28 09:50:36 +0600

    Part 04

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,44 @@
 <script setup>
-const tasks = [
-  { id: 1, title: "Complete project proposal", status: "In Progress" },
-  { id: 2, title: "Update website content", status: "Completed" },
-  { id: 3, title: "Fix bug in user authentication", status: "In Progress" },
-  { id: 4, title: "Prepare presentation slides", status: "Completed" },
-  { id: 5, title: "Test new feature implementation", status: "In Progress" },
-];
-
-function inProgressTask(){
-    return tasks.filter(task => 'In Progress'===task.status)
-}
-
-const person = {
-    Name: 'Momin Khan',
-    Age: 25,
-    Occupation: 'Web developer',
-    Email: 'mominkhan8828@gmail.com'
-
-}
-
+const persons = [
+  {
+    id: 1,
+    name: "Jane Doe",
+    title: "CEO-Founder",
+    img: "https://tailus.io/sources/blocks/classic/preview/images/woman1.jpg"
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    title: "Chief Technical Officer",
+    img: "https://tailus.io/sources/blocks/classic/preview/images/man.jpg"
+  },
+  {
+    id: 3,
+    name: "Jessy Doe",
+    title: "Chief Operations Officer",
+    img: "https://tailus.io/sources/blocks/classic/preview/images/woman.jpg"
+  },
+]
 </script>
 
 <template>
-  <section class="mx-auto container">
-    <section class="mx-auto container space-y-3">
-    <h1 class="text-2xl font-bold">All Tasks</h1>
-      <div
-        class="p-5 border border-gray-600 text-left"
-        v-for="task in tasks"
-        :key="task.id"
-        :class=" 'Completed'===task.status ? 'bg-red-300' : 'bg-sky-300' "
-      >
-       {{ task.id }} - {{ task.title }}
-      </div>
-
-      <h1 class="text-2xl font-bold mt-6">Complete Tasks</h1>
-      <div class="p-5 bg-red-300 text-left text-white" v-for="task in tasks" key="task.id" v-show=" 'Completed'===task.status ">{{ task.id }} - {{ task.title }}</div>
-
-      <h1 class="text-2xl font-bold mt-6">In Progress Tasks</h1>
-      <div class="p-5 bg-orange-300 text-left" v-for="task in inProgressTask()" key="task.id">{{ task.id }} - {{ task.title }}</div>
-
-      <h1 class="text-2xl font-bold mt-6">Object</h1>
-      <div class="p-5 bg-blue-300 text-left" v-for="(value, key, index) in person" :key="key"> {{ key }} = {{ value }} </div>
-
-    </section>
-  </section>
+  <div class="py-20 bg-gray-50">
+        <div class="container mx-auto px-6 px-32">
+            <div class="mb-16 text-center">
+                <h2 class="mb-4 text-center text-2xl text-gray-900 font-bold text-4xl">Team Member</h2>
+                <p class="text-gray-600 w-8/12 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni deleniti veniam laudantium itaque perferendis dolor maxime incidunt voluptatem. </p>
+            </div>
+            <div class="grid gap-12 items-center grid-cols-3">
+                <div class="space-y-4 text-center" v-for="person in persons" key="key.id">
+                    <img class="w-64 mx-auto object-cover rounded-xl w-64 h-64" :src="person.img" :class=" 2===person.id ? 'h-80' : 'h-64' ">
+                    <div>
+                        <h4 class="text-2xl">{{ person.name }}</h4>
+                        <span class="block text-sm text-gray-500">{{ person.title }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
